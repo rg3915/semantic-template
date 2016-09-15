ea9b8a7bd148ffa9108c7702975da572069b5d94
Eduardo Cuducos <cuducos@gmail.com>
2016-09-15 16:25:15 -0300

    Fix static files

--- semantic/settings.py
+++ semantic/settings.py
@@ -108,7 +108,7 @@ USE_THOUSAND_SEPARATOR = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'semantic', 'staticfiles'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'semantic', 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'semantic', 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = '/admin/login/'
